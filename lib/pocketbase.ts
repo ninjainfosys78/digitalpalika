ee3a65ab89ae29d8fee7a17873116b50246c9746
Riya Shrestha <ryeashrestha@gmail.com>
2025-12-05 17:53:47 +0545

    fix: pocketbase live url and config

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-const baseUrl = process.env.NEXT_PUBLIC_PB_URL || "http://127.0.0.1:8090";
+const baseUrl = process.env.NEXT_PUBLIC_PB_URL || "https://cms.ninjainfosys.com";
 
 const pb = new PocketBase(baseUrl);
 pb.autoCancellation(false);
