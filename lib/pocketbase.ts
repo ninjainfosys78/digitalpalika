b15779d599438e15e20759a7a1144d93f0272351
Abhi Pokhrel <abhipokhrel@ninjainfosys.com>
2025-12-05 06:47:17 +0000

    bug(ci): implement fix for non protected CI variable NEXT_PUBLIC_PB_URL

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -3,7 +3,6 @@ import PocketBase from "pocketbase";
 const baseUrl = process.env.NEXT_PUBLIC_PB_URL || "http://127.0.0.1:8090";
 
 const pb = new PocketBase(baseUrl);
-
 pb.autoCancellation(false);
 
 export default pb;
